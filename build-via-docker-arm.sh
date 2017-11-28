fa8712752264e77cb2c3ea1e999130e10f8fce12
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 00:56:39 +0000

    Updated build-via-docker-arm script

--- build-via-docker-arm.sh
+++ build-via-docker-arm.sh
@@ -1,2 +1,2 @@
 
-docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/duinocom.core compulsivecoder/ubuntu-arm-mono /bin/bash -c "cd /duinocom.core && sh prepare.sh && sh init.sh && sh build.sh"
+docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/duinocom.core compulsivecoder/ubuntu-arm-mono /bin/bash -c "cp -r /duinocom.core /project && cd /project && sh prepare.sh && sh init.sh && sh build.sh"
