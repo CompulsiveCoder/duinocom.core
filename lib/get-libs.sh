19550947eed08cc087bc52577e852b889c88c9dd
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 01:12:55 +0000

    Disabled nunit retrieval in get-libs because tests havent been implemented yet

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,17 +1,27 @@
 echo "Getting libraries for duinocom.core project"
 echo "Dir: $PWD"
 
-NUGET_FILE="nuget.exe"
+#NUGET_FILE="nuget.exe"
 
-if [ ! -f "$NUGET_FILE" ];
-then
-    wget http://nuget.org/nuget.exe
-fi
+#if [ ! -f "$NUGET_FILE" ];
+#then
+#    wget http://nuget.org/nuget.exe
+#fi
 
-if [ ! -d "NUnit.2.6.4" ]; then
-    mono nuget.exe install nunit.framework -version 2.6.4
-fi
+#if [ ! -d "NUnit.2.6.4" ]; then
+#    mono nuget.exe install nunit.framework -version 2.6.4
+#fi
 
-if [ ! -d "NUnit.Runners.2.6.4" ]; then
-    mono nuget.exe install nunit.runners -version 2.6.4
-fi
+#if [ ! -d "NUnit.Runners.2.6.4" ]; then
+#    mono nuget.exe install nunit.runners -version 2.6.4
+#fi
+
+#mono nuget.exe update -self
+
+#if [ ! -d "NUnit.3.7.0" ]; then
+#    mono nuget.exe install nunit -version 3.7.0
+#fi
+
+#if [ ! -d "NUnit.Console.3.7.0" ]; then
+#    mono nuget.exe install nunit.console -version 3.7.0
+#fi
