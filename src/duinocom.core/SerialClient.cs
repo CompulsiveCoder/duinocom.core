17fc1461aa9f298a0d75b84954f5ff0e323c0608
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-06 20:10:26 +1000

    Added ReadLine function

--- src/duinocom.core/SerialClient.cs
+++ src/duinocom.core/SerialClient.cs
@@ -6,59 +6,65 @@ namespace duinocom
 	public class SerialClient
 	{
 		public SerialPort Port { get; set; }
-        
-		public SerialClient (string portName, int baudRate)
+
+		public SerialClient(string portName, int baudRate)
 		{
 			Port = new SerialPort(portName, baudRate);
 		}
-		
-		public SerialClient (SerialPort port)
+
+		public SerialClient(SerialPort port)
 		{
 			Port = port;
 		}
-		
+
 		public void Open()
 		{
 			if (!Port.IsOpen)
-				Port.Open ();
+				Port.Open();
 		}
-		
+
 		public void Close()
 		{
-            Port.Close();
+			Port.Close();
 		}
-		
+
 		public void Write(string message)
 		{
-			Port.Write (message);
+			Port.Write(message);
 		}
-		
+
 		public void WriteLine(string message)
 		{
-			Port.Write (message);
-		
-			Port.Write (Port.NewLine);
+			Port.Write(message);
+
+			Port.Write(Port.NewLine);
 		}
-		
+
 		public string WriteAndRead(string message)
 		{
 			WriteLine(message);
-			
+
 			return Read();
 		}
-	
+
 		public string Read()
 		{
 			var returnMessage = String.Empty;
 			int count = Port.BytesToRead;
 			int intReturnASCII = 0;
-			while (count > 0) {
-				intReturnASCII = Port.ReadByte ();
-				returnMessage = returnMessage + Convert.ToChar (intReturnASCII);
+			while (count > 0)
+			{
+				intReturnASCII = Port.ReadByte();
+				returnMessage = returnMessage + Convert.ToChar(intReturnASCII);
 				count--;
 			}
 
 			return returnMessage.Trim();
+		}
+
+		public string ReadLine()
+		{
+			return Port.ReadLine();
 		}
 	}
 }
