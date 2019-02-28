e6ab36da156a533988ee3e175518451427f57364
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-28 03:52:03 +0000

    Updated scripts

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,27 +1,7 @@
-echo "Getting libraries for duinocom.core project"
-echo "Dir: $PWD"
+echo "Getting library files..."
+echo "  Dir: $PWD"
 
-#NUGET_FILE="nuget.exe"
+#sh install-package.sh NUnit 2.6.4
+#sh install-package.sh NUnit.Runners 2.6.4
 
-#if [ ! -f "$NUGET_FILE" ];
-#then
-#    wget http://nuget.org/nuget.exe
-#fi
-
-#if [ ! -d "NUnit.2.6.4" ]; then
-#    mono nuget.exe install nunit.framework -version 2.6.4
-#fi
-
-#if [ ! -d "NUnit.Runners.2.6.4" ]; then
-#    mono nuget.exe install nunit.runners -version 2.6.4
-#fi
-
-#mono nuget.exe update -self
-
-#if [ ! -d "NUnit.3.7.0" ]; then
-#    mono nuget.exe install nunit -version 3.7.0
-#fi
-
-#if [ ! -d "NUnit.Console.3.7.0" ]; then
-#    mono nuget.exe install nunit.console -version 3.7.0
-#fi
+echo "Finished getting library files."
