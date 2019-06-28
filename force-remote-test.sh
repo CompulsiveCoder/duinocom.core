dc65da84160c057a178af64bfcae49cd42c055c5
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-28 12:09:17 +1000

    Added force remote test script

---
+++ force-remote-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ "$BRANCH" = "dev" ]
+then
+  echo "Forcing remote test"
+
+  sh clean.sh || exit 1
+
+  echo " " >> Jenkinsfile
+  
+  git pull origin dev && \
+  git commit Jenkinsfile -m "Forcing retest" && \
+  git push origin dev && \
+  
+  echo "Repository has been updated. Test should now start on test server."
+else
+  echo "Cannot force retest from master branch. Switch to dev branch first."
+fi
